92c61a7e4d4d04d79f3a2160ad7186209d3fb3f8
mahamdakre283-eng <mahamdakre283@gmail.com>
2026-07-08 15:11:57 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration for Saman Cafeteria Menu
+// Uses Firebase Compat SDK (loaded via CDN in HTML)
+
+// Your web app's Firebase configuration
+window.firebaseConfig = {
+  apiKey: "AIzaSyDE_G0X7qZkXmP6bL2n6V4W3M8Y1PzQw5R",
+  authDomain: "menuu-54972.firebaseapp.com",
+  databaseURL: "https://menuu-54972-default-rtdb.firebaseio.com",
+  projectId: "menuu-54972",
+  storageBucket: "menuu-54972.firebasestorage.app",
+  messagingSenderId: "367351239102",
+  appId: "1:367351239102:web:04d2f8b5a03496bc123456"
+};
